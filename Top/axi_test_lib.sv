dd4a118344e486f73f47fc5562be06dd1431e88c
Atif Mughal <atifmughal62843@gmail.com>
2024-12-17 11:38:32 +0500

    Adding Concurrent Read Write Test Cases

--- Top/axi_test_lib.sv
+++ Top/axi_test_lib.sv
@@ -777,11 +777,12 @@ class axi_single_raw_incr_test extends axi_base_test;
     endtask: run_phase
 endclass: axi_single_raw_incr_test
 
+
 // ==========================================================================================
-//                              AXI SINGLE RAW WRAP TEST CLASS
+//                              AXI SINGLE CONCURRENT INCR TEST CLASS
 // ==========================================================================================
-class axi_single_raw_wrap_test extends axi_base_test;
-    `uvm_component_utils(axi_single_raw_wrap_test)
+class axi_single_concurrent_fixed_test extends axi_base_test;
+    `uvm_component_utils(axi_single_concurrent_fixed_test)
     axi_write_raw_sequence write_raw_seq;
     axi_read_raw_sequence read_raw_seq;
 
@@ -798,7 +799,7 @@ class axi_single_raw_wrap_test extends axi_base_test;
         test_cfg.burst_length = 8;
         test_cfg.burst_size = 2;
         test_cfg.ARESET_n = 1;     // Set reset signal to active (low)
-        test_cfg.burst_type = 2;
+        test_cfg.burst_type = 0;
     endfunction: new
 
     //---------------------------------------------------------------------------
@@ -826,9 +827,67 @@ class axi_single_raw_wrap_test extends axi_base_test;
     //---------------------------------------------------------------------------
     task run_phase(uvm_phase phase);
         phase.raise_objection(this);            // Raise objection to keep simulation running
-        write_raw_seq.start(env.master.my_agent.write_seqr); // Start write sequence
-        read_raw_seq.start(env.master.my_agent.read_seqr); // Start write sequence
+        fork
+		write_raw_seq.start(env.master.my_agent.write_seqr); // Start write sequence
+		read_raw_seq.start(env.master.my_agent.read_seqr); // Start write sequence
+        join
         phase.drop_objection(this);             // Drop objection after the sequence starts
     endtask: run_phase
-endclass: axi_single_raw_wrap_test
+endclass: axi_single_concurrent_fixed_test
+// ==========================================================================================
+//                              AXI SINGLE CONCURRENT INCR TEST CLASS
+// ==========================================================================================
+class axi_single_concurrent_incr_test extends axi_base_test;
+    `uvm_component_utils(axi_single_concurrent_incr_test)
+    axi_write_raw_sequence write_raw_seq;
+    axi_read_raw_sequence read_raw_seq;
+
+    //---------------------------------------------------------------------------
+    // Constructor:
+    // - Creates an instance of axi_single_raw_incr_test class.
+    //---------------------------------------------------------------------------
+    function new(string name, uvm_component parent);
+        super.new(name, parent);  // Call base class constructor
+        test_cfg = new("test_cfg");           // Create a new test configuration object
+        test_cfg.number_of_write_cases = 1;  // Set number of write cases
+        test_cfg.number_of_read_cases = 1;  // Set number of write cases
+        test_cfg.addr = 8;  // Set number of write cases
+        test_cfg.burst_length = 8;
+        test_cfg.burst_size = 2;
+        test_cfg.ARESET_n = 1;     // Set reset signal to active (low)
+        test_cfg.burst_type = 1;
+    endfunction: new
+
+    //---------------------------------------------------------------------------
+    // Build Phase:
+    // - Configures test settings for AXI write operation.
+    // - Instantiates write sequence for the test.
+    //---------------------------------------------------------------------------
+    function void build_phase(uvm_phase phase);
+        super.build_phase(phase);  // Invoke base class build phase
+        write_raw_seq = new("write_raw_seq");
+        read_raw_seq = new("read_raw_seq");
+    endfunction: build_phase
+
+    //---------------------------------------------------------------------------
+    // End of Elaboration Phase:
+    // - Completes the elaboration phase for the test.
+    //---------------------------------------------------------------------------
+    function void end_of_elaboration_phase(uvm_phase phase);
+        super.end_of_elaboration_phase(phase);  // Complete the elaboration phase
+    endfunction: end_of_elaboration_phase
+
+    //---------------------------------------------------------------------------
+    // Run Phase:
+    // - Starts the write sequence and raises/drops objections during the phase.
+    //---------------------------------------------------------------------------
+    task run_phase(uvm_phase phase);
+        phase.raise_objection(this);            // Raise objection to keep simulation running
+        fork
+		write_raw_seq.start(env.master.my_agent.write_seqr); // Start write sequence
+		read_raw_seq.start(env.master.my_agent.read_seqr); // Start write sequence
+        join
+        phase.drop_objection(this);             // Drop objection after the sequence starts
+    endtask: run_phase
+endclass: axi_single_concurrent_incr_test
 
